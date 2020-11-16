efea45f95e77f3af67aef0f65896b29069b75519
Lokeshvar <lokeshvarsakthi@gmail.com>
2020-11-16 07:31:49 +0530

    setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='backend_api',
+    version='',
+    packages=['api', 'api.migrations', 'backend_api'],
+    url='',
+    license='',
+    author='HP',
+    author_email='',
+    description=''
+)
