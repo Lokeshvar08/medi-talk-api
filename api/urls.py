f4cc598e5abadefba52f9ac5b9c66c87f3af525a
Lokeshvar <lokeshvarsakthi@gmail.com>
2020-11-16 08:26:08 +0530

    Create Doctor

--- api/urls.py
+++ api/urls.py
@@ -6,4 +6,5 @@ from api import views
 urlpatterns = [
     path('doctors/<str:specialisation>/', views.get_doctor_location, name='get-doctors-location'),
     path('doctors/<str:location>/<str:specialisation>/',views.get_doctor_location_specialisation,name='get-doctor-narrow'),
+    path('doctor-create/',views.create_doctor,name='create-doctor'),
 ]
--- api/views.py
+++ api/views.py
@@ -20,3 +20,11 @@ def get_doctor_location_specialisation(request, location, specialisation):
     doctors = Doctor.objects.filter(location=location, specialisation=specialisation).order_by('-average_rating')
     serializer = DoctorSerializer(doctors, many=True)
     return Response(serializer.data, status=200)
+
+
+@api_view(['POST'])
+def create_doctor(request):
+    serializer = DoctorSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
